5948ba0985b3b6ea2a9da837fcd7f93804289736
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-06-03 23:33:44 +0530

    Dynamic Routing With Param

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <p>We Good Planner and Fly Like eagle</p>
+
+      <Link to="/user/anil">Anil</Link>
+      <Link to="/user/peter">Peter</Link>
     </div>
   );
 };
