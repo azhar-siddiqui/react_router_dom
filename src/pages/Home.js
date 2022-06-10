7d4414bdf2d56966faccc610e994446dcdbb9313
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-06-10 23:54:25 +0530

    Navigate On Click

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const navigation = () => {
+    navigate("/about");
+    navigate("/filter");
+  };
+  // conditional
+  // const navigation = () => {
+  //   let x = true;
+  //   if (x) {
+  //     navigate("/about");
+  //   } else {
+  //     navigate("/filter");
+  //   }
+  // };
   return (
     <div>
       <h1>Home</h1>
+      <NavLink to="/about">Go to About</NavLink>
+      <button onClick={navigation}>Go to About</button>
+      <button onClick={navigation}>Go to Filter</button>
     </div>
   );
 };
